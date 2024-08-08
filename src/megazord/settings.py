c7401f36b713974ece7d2fc03c9c3d366989c1a2
Alex <pozitiv870@gmail.com>
2024-08-08 15:19:51 +0800

    Add: funcs for tg reg

--- src/megazord/settings.py
+++ src/megazord/settings.py
@@ -143,6 +143,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'pozitiv870@gmail.com'
 EMAIL_HOST_PASSWORD = 'kjvd vbfr wapa qpdu'
 
