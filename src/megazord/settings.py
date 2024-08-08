d386a325a7b02cd575dba6f293bea05d70c692bc
Pozitiv4500 <123630113+Pozitiv4500@users.noreply.github.com>
2024-08-08 20:26:38 +0800

    DELL: info settings.py

--- src/megazord/settings.py
+++ src/megazord/settings.py
@@ -13,17 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
-import environ
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-env = environ.Env()
-env.read_env(BASE_DIR.parent / ".env")
-
-# Настройки электронной почты
-
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -149,15 +138,13 @@ STATICFILES_DIRS = (
 #     'django.contrib.auth.backends.ModelBackend',
 # ]
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-
-EMAIL_HOST = env.int('EMAIL_HOST', default='smtp.gmail.com')
-EMAIL_PORT = env.int('EMAIL_PORT', default=587)
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 
 
