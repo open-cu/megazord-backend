a58c6d46c067b4f30ffd734b874c6628e01d88cd
Alex <pozitiv870@gmail.com>
2024-08-20 05:58:01 +0800

    back for reg tg

--- src/accounts/api.py
+++ src/accounts/api.py
@@ -1,4 +1,5 @@
 from django.contrib import auth
+from django.shortcuts import get_object_or_404
 from ninja import Router
 from ninja.errors import HttpError
 
@@ -20,7 +21,6 @@ router = Router()
 def signup(
     request: APIRequest, schema: RegisterSchema
 ) -> tuple[int, RegisterResponseSchema]:
-    # Проверка на наличие запятых в имени пользователя
     if "," in schema.username:
         raise HttpError(422, "Username cannot contain commas.")
 
@@ -53,3 +53,30 @@ def signin(request: APIRequest, schema: LoginSchema) -> tuple[int, TokenSchema]:
 
     token = create_jwt(user_id=account.id)
     return 200, TokenSchema(token=token)
+
+
+@router.post(
+    path="/link_telegram",
+    summary="Link Telegram ID",
+    response={200: dict, 404: ErrorSchema},
+)
+def link_telegram(request: APIRequest, user_uuid: str, telegram_id: str):
+    user = get_object_or_404(Account, uuid=user_uuid)
+
+    user.telegram_id = telegram_id
+    user.save()
+
+    return {"detail": "Telegram ID привязан успешно"}
+
+
+@router.get(
+    path="/generate_telegram_link",
+    summary="Generate Telegram Link",
+    response={200: dict, 404: ErrorSchema},
+)
+def generate_telegram_link(request: APIRequest, email: str) -> dict:
+    user = get_object_or_404(Account, email=email)
+
+    telegram_link = f"https://t.me/FindYourMate_bot?start={str(user.uuid)}"
+
+    return {"telegram_link": telegram_link}
