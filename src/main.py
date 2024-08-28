3c5a07578b1c3d38cd5d7c9c1b247c92f40755b4
lysmux <kllraz@yandex.ru>
2024-08-28 22:18:55 +0300

    setup deploy

--- src/main.py
+++ src/main.py
@@ -5,6 +5,6 @@ from megazord import settings
 uvicorn.run(
     app="megazord.asgi:application",
     reload=settings.RELOAD,
-    host="0.0.0.0",
-    port=8000,
+    host=settings.SERVER_HOST,
+    port=settings.SERVER_PORT,
 )
